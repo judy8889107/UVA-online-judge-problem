cd14f184802e00da015744ca76aea28635711807
Judy Yen <judy8889107@gmail.com>
2021-12-17 22:25:54 +0800

    1

---
+++ UVA10026 - Shoemaker's Problem/UVA10026 - Shoemaker's Problem.cpp
@@ -0,0 +1,48 @@
+//p10026
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+struct Job{
+	float weight=0; //fine/time
+	int index=0; //The i-th job
+};
+
+bool compare(Job a, Job b){
+	return a.weight>b.weight; //To sort from large to small.
+}
+
+int main(){
+	
+	int test_case;
+	cin >> test_case;
+	
+	while(test_case--){
+			int n;
+			cin >> n;
+			float time, fine;
+			Job job[n];
+			
+			for(int i=0;i<n;i++){
+				cin >> time >> fine;
+				job[i].weight = fine/time;
+				job[i].index = i+1; 
+			}
+			
+			sort(job,job+n,compare);
+			
+			for(int i=0;i<n;i++){
+				cout << job[i].index;
+				if(i!=n-1) cout << " ";
+			}
+			cout << endl;
+			if(test_case!=0) cout << endl;
+			
+			
+			
+			
+			
+		
+	}
+}
